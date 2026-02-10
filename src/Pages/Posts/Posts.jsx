9a1b8ddeaf7c1908e3861aea1970434472d94fbb
gamastronger <refasetyagama@gmail.com>
2026-02-10 16:58:29 +0700

    feat: Posts useEffect

--- src/Pages/Posts/Posts.jsx
+++ src/Pages/Posts/Posts.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Posts() {
-    const [post, setPost] = useState([
-        { id: 1, title: "post pertama" },
-        { id: 2, title: "post kedua" },
-        
-    ]);
+    const [post, setPost] = useState([])
 
+    useEffect(() => {
+        setPost([
+            { id: 1, title: "pertama" },
+            { id: 2, title: "kedua" },
+        ])
+    }, [])
+        
     return (
         <div style={{padding: "2rem"}}>
             <h1>Post Page</h1>
